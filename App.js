112b90fac0f217b001a0fa286fb230169c046243
Chao Hsien Li <taipeisky101@gmail.com>
2018-08-14 13:51:25 -0700

    can fetch api now

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+fetchData = async () => {
+  const response = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=4679f09b18b0eeea0593c6287ae8a4e1&language=en-US&page=1");
+  const json = await response.json();
+  this.setState({ data: json.results });
+};
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <View style={styles.container}>
+        <FlatList data={this.state.data} keyExtractor={(x, i) => i} renderItem={({ item }) => <Text>
+              {item.id} {item.title}
+            </Text>} />
+      </View>;
   }
 }
 
